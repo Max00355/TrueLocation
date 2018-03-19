745334c06e5d2c9391e259b45c9514c4e2453951
Frankie <frankieprimerano@gmail.com>
2018-03-18 20:38:17 -0400

    Initial Commit

---
+++ Truloc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Truloc
+//
+//  Created by Frankie Primerano on 3/18/18.
+//  Copyright © 2018 Frankie Primerano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
