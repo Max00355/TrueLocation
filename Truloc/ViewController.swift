c0311da1bc206debfa1a4dfc00f5a85f0d24e809
Frankie <frankieprimerano@gmail.com>
2018-03-21 12:31:44 -0400

    Using elevation (height above sea level) instead of relative altitude

--- Truloc/ViewController.swift
+++ Truloc/ViewController.swift
@@ -9,36 +9,21 @@
 import UIKit
 import MapKit
 import CoreLocation
-import CoreMotion
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var map: MKMapView!
 
     var locationManager: CLLocationManager!
-    var altitudeManager: CMAltimeter!
-    var relativeAltitude: Float?
     var currentCoordinates: CLLocationCoordinate2D?
+    var altitude: Double?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.startGetCoordinates()
-        self.startGetRelativeAltitude()
+        self.startTrackingLocation()
     }
     
-    
-    func startGetRelativeAltitude() {
-        altitudeManager = CMAltimeter()
-        if(CMAltimeter.isRelativeAltitudeAvailable()) {
-            altitudeManager.startRelativeAltitudeUpdates(to: OperationQueue.current!, withHandler: { (data, error) in
-                let relativeAltitude = data?.relativeAltitude.floatValue
-                self.relativeAltitude = relativeAltitude
-                print(relativeAltitude!)
-            })
-        }
-    }
-    
-    func startGetCoordinates() {
+    func startTrackingLocation() {
         self.locationManager = CLLocationManager()
         self.locationManager.delegate = self
         self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
@@ -53,9 +38,12 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let latitude = locations.last!.coordinate.latitude
-        let longitude = locations.last!.coordinate.longitude
+        let longitude = locations.last!.coordinate.longitude // Latitude 
+        let altitude = locations.last!.altitude // Height above Sea Level
         self.currentCoordinates = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-        print("\(latitude) \(longitude)")
+        self.altitude = altitude
+        locations.last!.
+        print("\(latitude) \(longitude) \(altitude)")
     }
     
 }
